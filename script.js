d3bf800a22a092eeaa22da4f724ceaf9a21c9e1f
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-09-15 10:50:34 -0500

    Add reset sketchpad button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const resetBtn = document.querySelector('.btn__reset');
 
 const addGrid = () => {
   for (i = 0; i <= 255; i++) {
@@ -6,7 +7,7 @@ const addGrid = () => {
     const newDiv = document.createElement('div');
 
     // Add box style to newDiv
-    newDiv.classList.add('grid_cell');
+    newDiv.classList.add('grid__cell');
 
     // add to OG DIV in HTML
     container.append(newDiv);
@@ -17,7 +18,14 @@ document.body.onload = addGrid;
 
 container.addEventListener('mouseover', e => {
   console.log(e.target);
-  if (e.target.className === 'grid_cell') {
+  if (e.target.className === 'grid__cell') {
     e.target.classList.add('hover');
   }
+});
+
+resetBtn.addEventListener('click', () => {
+  const grid = document.querySelectorAll('.grid__cell');
+  grid.forEach(cell => {
+    cell.classList.remove('hover');
+  });
 });
